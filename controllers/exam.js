9cd13d6c43bbc0715a714842585c8a4c4a64de3a
Black Lee <webitlee@163.com>
2018-01-04 18:01:50 +0800

    添加了获取考题列表，修改考题，上传考题的数据接口

--- controllers/exam.js
+++ controllers/exam.js
@@ -33,7 +33,7 @@ var add_exam = async (ctx, next)=>{
             responseData.message = '考题范围不合法';
             throw new Error();
         }
-        exam_dao.add_exam(title, content, options, type, scope);
+        await exam_dao.add_exam(title, content, options, type, scope);
         responseData.message = '考题添加成功';
         ctx.response.body = responseData;
         await next();
@@ -43,6 +43,104 @@ var add_exam = async (ctx, next)=>{
     }
 }
 
+//获取所有考题
+var get_exams = async(ctx, next)=>{
+    var responseData = {
+        message : '获取考题列表失败'
+    }
+    try{
+        let exams = null; 
+        let offset = ctx.request.body.offset;
+        let limit = ctx.request.body.limit;
+        if(offset < 0 || isNaN(offset)){
+            responseData.message = '页码数不合法';
+            throw new Error();
+        }
+        if(limit <=0 || isNaN(limit)){
+            responseData.message = '每页数据条数不合法';
+            throw new Error();
+        }
+        await exam_dao.get_exams(offset, limit).then((result)=>{
+            exams = JSON.parse(result);
+        })
+        console.log(exams);
+        ctx.response.body = exams;
+        await next();
+    }catch(err){
+        console.error(responseData.message);
+        ctx.response.status = 500;
+    }
+}
+//根据id获取指定考题
+var get_exam = async(ctx, next)=>{
+    var responseData = {
+        message : '获取指定考题失败'
+    }
+    try{
+        let exam = null;
+        let id = ctx.request.body.id;
+        if(id <= 0 || isNaN(id)){
+            responseData.message = '考题id不合法';
+            throw new Error();
+        }
+        await exam_dao.get_exam(id).then((result)=>{
+            exam = JSON.parse(result);
+        })
+        console.log(exam);
+        ctx.response.body = exam;
+        await next();
+    }catch(err){
+        console.error(responseData.message);
+        ctx.response.status = 500;
+    }
+}
+
+//修改指定考题
+var update_exam = async (ctx, next)=>{
+    var responseData = {
+        message : '修改指定考题失败'
+    };
+    try{
+        let id = ctx.request.body.id;
+        let title = ctx.request.body.title;
+        let content = ctx.request.body.content;
+        let options = ctx.request.body.options;
+        let type = ctx.request.body.type;
+        let scope = ctx.request.body.scope;
+        if(id <= 0 || isNaN(id)){
+            responseData.message = '考题id不合法';
+            throw new Error();
+        }
+        if(!content){
+            responseData.message = '考题内容未填写';
+            throw new Error();
+        }
+        if(options.length <= 1){
+            responseData.message = '考题选项数量最少2个';
+            throw new Error();
+        }
+        if(type <= 0 || isNaN(type)){
+            responseData.message = '考题类型不合法';
+            throw new Error();
+        }
+        if(scope <= 0 || isNaN(scope)){
+            responseData.message = '考题范围不合法';
+            throw new Error();
+        }
+        await exam_dao.update_exam(id, title, content, options, type, scope);
+        responseData.message = '考题修改成功';
+        ctx.response.body = responseData;
+        console.log(1112);
+        await next();
+    }catch(err){
+        console.error(responseDate.message);
+        ctx.response.status = 500;
+    }
+}
+
 module.exports = {
-    'post /addExam' : add_exam
+    'post /addExam' : add_exam,
+    'post /getExams' : get_exams,
+    'post /getExam' : get_exam,
+    'post /updateExam' : update_exam
 }
--- dao/exam.js
+++ dao/exam.js
@@ -2,6 +2,7 @@
 const model = require('../import-middleware')();
 const exam = model.exam;
 const answer = model.answer;
+const scope = model.scope;
 
 //添加考题
 function add_exam(title, content, options, type, scope){
@@ -12,7 +13,6 @@ function add_exam(title, content, options, type, scope){
         }
         answers.push(obj);
     }
-    console.log(answers);
     exam.create({
         title,
         content,
@@ -24,6 +24,68 @@ function add_exam(title, content, options, type, scope){
     })
 }
 
+//获取所有考题
+async function get_exams(offset, limit){
+    var exams = [];
+    await exam.findAndCountAll({
+        limit,
+        offset,
+        include : {
+            model : scope
+        }
+    }).then((result)=>{
+        exams = JSON.stringify(result);
+    })
+    return exams;
+}
+//根据id获取指定考题
+async function get_exam(id){
+    var data = null;
+    await exam.findById(id, {
+        include : [
+            {
+                model : scope
+            },
+            {
+                model : answer
+            }
+        ]
+    }).then((result)=>{
+        data = JSON.stringify(result);
+    })
+    return data;
+}
+
+//修改指定考题
+async function update_exam(id, title, content, options, type, chooseScope){
+    await exam.findById(id, {
+        include : [
+            {
+                model : scope
+            },
+            {
+                model : answer
+            }
+        ]
+    }).then((result)=>{
+        console.log(JSON.stringify(result));
+        result.title = title;
+        result.content = content;
+        answers = [];
+        result.setAnswers([]);
+        for(let i = 0; i < options.length; i++){
+            var newAnswer = answer.create({option : options[i], exam_id : id});
+            result.addAnswers(answers);
+        }
+        result.type = type;
+        result.scope_id = chooseScope;
+        result.save();
+    })
+}
+
 module.exports = {
-    add_exam
+    add_exam,
+    get_exams,
+    get_exam,
+    update_exam
 }
